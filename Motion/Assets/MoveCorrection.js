41f5f9469fb673f6dfef05d62b5566e20e4526ff
KurtGodel <mtredding@yahoo.com>
2015-04-26 07:26:33 -0500

    MoveCorrection.js added

--- Motion/Assets/MoveCorrection.js
+++ Motion/Assets/MoveCorrection.js
@@ -1,9 +1,19 @@
 ﻿#pragma strict
 
-function Start () {
+var positionLastFrame;
 
+function Start () {
+	positionLastFrame = gameObject.transform.position
 }
 
 function Update () {
-
+	var pos = gameObject.transform.position;
+	var dumbWalk = pos - positionLastFrame;
+	
+	var k = 4;
+	var positionToAdd = Vector3(k*dumbWalk);
+	var newPos = gameObject.transform.position + positionToAdd;
+	gameObject.transform.position = newPos;
+	
+	positionLastFrame = pos;
 }
